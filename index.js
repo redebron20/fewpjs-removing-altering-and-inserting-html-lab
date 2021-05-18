42c04625fa50273b17a8aee8e1af79d9ff6cc65a
redebron20 <redebron20@gmail.com>
2021-05-18 10:57:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1"); 
+document.body.appendChild(newHeader);
+newHeader.id = "victory"
+newHeader.innerHTML = "Red is the champion!"
